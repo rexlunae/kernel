1ec91ac4bbc6d93761b0f3c7cadec6aac4269c70
Rex Lunae <rexlunae@gmail.com>
2016-10-09 18:06:32 -0700

    Added multibook struct and handling.

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -6,8 +6,8 @@ mod x86_64;
 type CPU = x86_64::CPU;
 
 // Initialize the architecture-specific features.
-pub fn init() -> CPU {
-	let cpu = CPU::init();
+pub fn init(boot_info_struct: usize) -> CPU {
+	let cpu = CPU::init(boot_info_struct);
 	
 	cpu
 }
--- src/arch/x86_64/mod.rs
+++ src/arch/x86_64/mod.rs
@@ -1,19 +1,32 @@
 // X86_64-specific code.
 extern crate x86;
+extern crate multiboot2;
 
+//use self::multiboot2;
 use self::x86::shared::halt;
 
 pub struct CPU {
 	pub arch: &'static str,
+	pub memory_areas: &'static multiboot2::MemoryMapTag,
 }
 
 impl CPU {
-	pub fn init() -> Self {
-		CPU { arch: "x86_64" }
+	pub fn init(boot_info_struct: usize) -> Self {
+		let boot_info = unsafe { multiboot2::load(boot_info_struct) };
+		let memory_map_tag = boot_info.memory_map_tag()
+			.expect("Memory map tag required");
+
+		for area in memory_map_tag.memory_areas() {
+			println!("memory region 0x{:x} length 0x{:x}", area.base_addr, area.length);
+		}
+			
+		CPU { arch: "x86_64", memory_areas: memory_map_tag }
 	}
 	
 	#[inline(always)]
 	pub fn halt() {
 		unsafe { halt(); }
 	}
+	
+	
 }
