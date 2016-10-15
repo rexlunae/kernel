fc923c40effc744746729768b81a2714331952a1
Rex Lunae <rexlunae@gmail.com>
2016-10-14 19:11:09 -0700

    Initial commit.

--- src/arch/x86_64/mod.rs
+++ src/arch/x86_64/mod.rs
@@ -8,19 +8,25 @@ use self::x86::shared::halt;
 pub struct CPU {
 	pub arch: &'static str,
 	pub memory_areas: &'static multiboot2::MemoryMapTag,
+	pub elf_sections: &'static multiboot2::ElfSectionsTag,
 }
 
 impl CPU {
 	pub fn init(boot_info_struct: usize) -> Self {
 		let boot_info = unsafe { multiboot2::load(boot_info_struct) };
+
 		let memory_map_tag = boot_info.memory_map_tag()
 			.expect("Memory map tag required");
-
 		for area in memory_map_tag.memory_areas() {
 			println!("memory region 0x{:x} length 0x{:x}", area.base_addr, area.length);
 		}
-			
-		CPU { arch: "x86_64", memory_areas: memory_map_tag }
+
+		let elf_sections = boot_info.elf_sections_tag()
+			.expect("Elf-sections tag required");
+		for section in elf_sections.sections() {
+			println!("kernel section 0x{:x}, size 0x{:x}, flags 0x{:x}", section.addr, section.size, section.flags)
+		}
+		CPU { arch: "x86_64", memory_areas: memory_map_tag, elf_sections: elf_sections }
 	}
 	
 	#[inline(always)]
