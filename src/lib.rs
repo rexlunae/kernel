7d84a5384214a38aeabee59817108ebc484a5c69
Rex Lunae <rexlunae@gmail.com>
2017-04-20 19:45:26 -0700

    Added new features.

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,58 @@
+#![feature(allocator)]
 #![feature(lang_items)]
 #![feature(const_fn)]
 #![feature(unique)]
+#![feature(abi_x86_interrupt)]
 #![no_std]
 extern crate rlibc;
 extern crate spin;
 //extern crate time;
 
 #[macro_use]
+extern crate lazy_static;
+
+
+#[macro_use]
 extern crate log;
+
+#[macro_use]
+//extern crate alloc_no_stdlib;
+extern crate linked_list_allocator;
+
 
 //use time::now;
 
 pub mod syslog;
 pub mod term;
+pub mod memory;
+pub mod process;
+//pub mod interrupts;
 
 // arch is what gives us a console to write to.
 #[macro_use]
 pub mod arch;
 
+// This function runs unprivileged.
+pub extern "C" fn userland_main() {
+
+
+}
+
+impl arch::Events {
+    pub fn test() {
+    }
+}
 
 #[no_mangle]
 pub extern fn rust_main(boot_info_struct: usize) {
+    let sys = arch::init(boot_info_struct);
     //let boot_time = now();
-    let cpu = arch::init(boot_info_struct);
-    println!("CPU Architecture: {}", cpu.arch);
-    println!("Boot struct located at {}", boot_info_struct);
+    println!("CPU Architecture: {}", sys.arch);
+    println!("Boot struct located at 0x{:x}", boot_info_struct);
     
     // Main Kernel loop
     loop {
-		unsafe { arch::halt(); }
+		arch::halt();
 		println!("Cycle!");
     }
 }
@@ -37,4 +61,4 @@ pub extern fn rust_main(boot_info_struct: usize) {
 #[allow(non_snake_case)]
 #[no_mangle] pub fn _Unwind_Resume() -> ! { loop {} }
 #[lang = "eh_personality"] extern fn eh_personality() {}
-#[lang = "panic_fmt"] extern fn panic_fmt() -> ! { loop{} }
+#[no_mangle] #[lang = "panic_fmt"] extern fn panic_fmt() -> ! { println!("KERNEL PANIC!!!"); loop{} }
